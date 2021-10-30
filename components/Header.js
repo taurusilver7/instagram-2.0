b287e3159c57cb36bbadf15c16e73154b7bb9793
taurusilver7 <prasad20697@gmail.com>
2021-10-30 21:30:01 +0530

    logo added

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
-const Header = () => {
-    return (
-        <div>
-            <h1>Header components</h1>
-        </div>
-    )
-}
+import Image from "next/image";
 
-export default Header
+const Header = () => {
+  return (
+    <div>
+      <h1>Header components</h1>
+
+      {/* left */}
+      <div>
+        <div>
+          <Image src="https://links.papareact.com/ocw" layout="fill" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
