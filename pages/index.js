12485c87ca244e044a5a018c33f24a49b99aec13
taurusilver7 <prasad20697@gmail.com>
2021-10-30 20:07:35 +0530

    Header added to index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -8,17 +9,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Instagram 2.0
-          </a>
-        </h1>
         {/* Header */}
+        <Header />
+
         {/* Feed */}
+
+        
         {/* Modal */}
-      </main>
     </div>
   );
 }
