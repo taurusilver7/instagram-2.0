7989062e9c1bca634742e609fa27e63711908929
taurusilver7 <prasad20697@gmail.com>
2021-10-31 18:36:39 +0530

    logo breakpoints added

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,20 @@ const Header = () => {
     <div>
       {/* left */}
       <div className="flex justify-between max-w-6xl">
-        <div className="relative h-24 w-24">
-          <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain" />
+        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
 
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
       </div>
     </div>
