d0bf80dc333404c2c458e1af500020cd6c536d12
taurusilver7 <prasad20697@gmail.com>
2021-11-01 14:08:54 +0530

    header styling

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
     <div>
       {/* left */}
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
         <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
@@ -13,12 +22,33 @@ const Header = () => {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 h-10 mt-6 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
             objectFit="contain"
           />
+        </div>
+
+        {/* Middle Search input field */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-7 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right options */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="w-10 h-10" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <PaperAirplaneIcon />
         </div>
       </div>
     </div>
