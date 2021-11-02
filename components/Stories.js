1184dbda6f74afaeeb90a1e1ed44d6003ddf1a14
taurusilver7 <prasad20697@gmail.com>
2021-11-02 19:42:23 +0530

    story added to comp

--- components/Stories.js
+++ components/Stories.js
@@ -1,7 +1,26 @@
+import faker from "faker";
+import { useEffect, useState } from "react";
+import Story from "./Story";
+
 const Stories = () => {
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    console.log(suggestions);
+    setSuggestions(suggestions);
+  }, []);
   return (
-    <div>
-      <h1>Stories</h1>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 rounded-sm overflow-x-scroll">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
   );
 };
