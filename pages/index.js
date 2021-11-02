076f974e97700437ace02146c42709229becbcb7
taurusilver7 <prasad20697@gmail.com>
2021-11-02 21:44:09 +0530

    scrollbar config chages styles

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-50 h-screen overflow-y-scroll">
       <Head>
         <title>Instagram 2.0</title>
         <link rel="icon" href="/favicon.ico" />
