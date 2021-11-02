77f239aa6503dda359a471696fc24f2121563bdd
taurusilver7 <prasad20697@gmail.com>
2021-11-02 21:55:21 +0530

    Posts component added to feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,3 +1,4 @@
+import Posts from "./Posts";
 import Stories from "./Stories";
 
 const Feed = () => {
@@ -8,6 +9,7 @@ const Feed = () => {
         <Stories />
 
         {/* Posts */}
+        <Posts />
       </section>
 
       <section>
