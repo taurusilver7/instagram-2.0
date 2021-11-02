8c434c93ae0ac86305c2bf779b5e69b8d7a23227
taurusilver7 <prasad20697@gmail.com>
2021-11-02 16:26:15 +0530

    comp to carry stories and posts

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+const Feed = () => {
+  return (
+    <main>
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions. */}
+      </section>
+    </main>
+  );
+};
+
+export default Feed;
