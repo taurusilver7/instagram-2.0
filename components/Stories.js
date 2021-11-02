52fbb63d0d3a898ecfca8dc626fcc10e70d2efa0
taurusilver7 <prasad20697@gmail.com>
2021-11-02 21:45:15 +0530

    scrollbar depen added to styles

--- components/Stories.js
+++ components/Stories.js
@@ -13,7 +13,7 @@ const Stories = () => {
     setSuggestions(suggestions);
   }, []);
   return (
-    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 rounded-sm overflow-x-scroll">
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
