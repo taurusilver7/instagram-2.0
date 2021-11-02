f32216e7adb633fef42fc34ec874c2f0a6a9e781
taurusilver7 <prasad20697@gmail.com>
2021-11-02 16:16:15 +0530

    anime bounce added

--- components/Header.js
+++ components/Header.js
@@ -48,7 +48,12 @@ const Header = () => {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <PaperAirplaneIcon className="navBtn" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-bounce text-white">
+              3
+            </div>
+          </div>
           <PlusCircleIcon className="navBtn" />
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
