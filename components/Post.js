c079a8ba676d2cc94ba907261b73101287ec069d
taurusilver7 <prasad20697@gmail.com>
2021-11-03 12:37:27 +0530

    head img and button added

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,47 @@
-const Post = () => {
-    return (
-        <div>
-            <h1>I'm a post in the feed</h1>
-        </div>
-    )
-}
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
-export default Post
+const Post = ({ id, username, userImg, img, caption }) => {
+  return (
+    <div className="bg-white my-7 border rounded-sm">
+      {/* Header */}
+      <div className="flex item-center p-3">
+        <img
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          src={userImg}
+          alt=""
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* img */}
+      <img className="object-cover w-full" src={userImg} alt="" />
+
+      {/* buttons */}
+      <div className="flex justify-between">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
+      {/* captions */}
+
+      {/* comments */}
+
+      {/* input box */}
+    </div>
+  );
+};
+
+export default Post;
