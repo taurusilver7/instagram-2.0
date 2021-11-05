6845e2e7667dcb990a095723fbad649ee098fece
taurusilver7 <prasad20697@gmail.com>
2021-11-05 17:42:30 +0530

    signin customized

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,28 @@ const signIn = ({ providers }) => {
   return (
     <>
       <Header />
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign In with {provider.name}
-          </button>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 px-14 -mt-32 text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw" />
+        <p className="font-xs italic">
+          This is not the genuine{" "}
+          <span className="font-semibold">Instagram</span>. Ony for educational
+          purpose.
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-300 rounded-lg text-white"
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign In with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
