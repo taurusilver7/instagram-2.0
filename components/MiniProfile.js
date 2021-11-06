b9eb1efe111da7bae7fef2e6c8579c869c3d57db
taurusilver7 <prasad20697@gmail.com>
2021-11-06 18:52:44 +0530

    miniprofile updated

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,15 +1,22 @@
+import { signOut, useSession } from "next-auth/react";
+
 const MiniProfile = () => {
+  const { data: session } = useSession();
+
+  // console.log(session);
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="w-16 h-16 rounded-full border p-[2px]"
-        src="https://static.wikia.nocookie.net/57cca220-437f-4ce2-b451-d1963591ab16"
+        src={session?.user?.image}
       />
       <div className="flex-1 mx-4">
-        <h2 className="font-bold">taurusilver</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button className="text-blue-400 text-sm font-semibold">Signout</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+        Signout
+      </button>
     </div>
   );
 };
