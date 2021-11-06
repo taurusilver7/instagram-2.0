6ff37214c3db0e362ec5ea29d147df001001f414
taurusilver7 <prasad20697@gmail.com>
2021-11-06 19:02:16 +0530

    conditional render added miniprofile & suggestions

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,11 @@
+import { useSession } from "next-auth/react";
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 import Suggestions from "./Suggestions";
 
 const Feed = () => {
+  const { data: session } = useSession();
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       <section className="col-span-2">
@@ -14,12 +16,14 @@ const Feed = () => {
         <Posts />
       </section>
 
-      <section className="hidden xl:inline-grid md:col-span-1">
-        <div className="fixed">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden xl:inline-grid md:col-span-1">
+          <div className="fixed">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   );
 };
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -3,7 +3,7 @@ import { signOut, useSession } from "next-auth/react";
 const MiniProfile = () => {
   const { data: session } = useSession();
 
-  // console.log(session);
+  console.log(session);
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
