7f41ca77d972fdd564a27af43c71e23c10c8acc6
taurusilver7 <prasad20697@gmail.com>
2021-11-23 23:23:53 +0530

    modal styling implemented

--- components/Modal.js
+++ components/Modal.js
@@ -153,7 +153,7 @@ const Modal = () => {
                 <button
                   type="button"
                   onClick={uploadPost}
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed"
                 >
                   {loading ? "Uploading..." : "Upload Post"}
                 </button>
